600556f1dd5b32130b72c4f46f30fc8741b6e876
田中冬紀 <winter.tanaka@gmail.com>
2023-11-27 14:36:38 +0900

    「部分和問題 4」追加、自分で解いたコード（失敗例）も追加

---
+++ dp_primer_partial_sums_boss.java
@@ -0,0 +1,59 @@
+/*
+  問題：
+  1 ~ n の番号がついた n 種類のおもりがあり、おもり i の重さは a_i です。
+  それぞれのおもりは無限個存在しており、任意のおもりを任意の個数使うことができます。
+
+  このとき、おもりを選んで重さの和を x となるようにすることができるかどうか判定してください。
+*/
+
+/*
+  解説：
+  おもり k までを用いて重さの和が x となるようにおもりを選ぶことができるかどうかがわかっていれば、
+  おもり k+1 までを用いて重さの和が x となるようにおもりを選ぶことができるかどうかがわかりますから、おもり 1 から始めて順におもり n まで計算していけばよいです。
+
+  dp_k[x] を、おもり k までを用いて重さの和が x となるようにおもりを選ぶことができるかどうかを表す真偽値とすると、
+  dp_{k-1}[0] ~ dp_{k-1}[x], dp_k[0] ~ dp_k[y (< x)] と dp_k[x] の関係は 
+  dp_k[x] = dp_{k-1}[x] or dp_{k-1}[x-a_k] or dp_k[x-a_k] となります。
+
+  dp_1 から dp_k に対応する k 本の1次元配列 (もしくはこれに相当する2次元配列) を用意し、上の漸化式に従って問題を解いても良いのですが、
+  部分和問題 1 で考察したようにループの回し方を工夫すると1本の1次元配列でこの問題を解くことができます。
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class dp_primer_partial_sums_boss {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int x = sc.nextInt();
+
+    int a[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+
+    boolean[] dp = new boolean[x + 1];
+
+    Arrays.fill(dp, false);
+    dp[0] = true;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = a[i]; j <= x; j++) {
+        if (dp[j - a[i]]) {
+          dp[j] = true;
+        }
+      }
+    }
+
+    if (dp[x]) {
+      System.out.println("yes");
+    } else {
+      System.out.println("no");
+    }
+
+    sc.close();
+  }
+}
---
+++ dp_primer_partial_sums_boss_NG.java
@@ -0,0 +1,54 @@
+/* 自分で解いてみたコード。失敗 */
+
+/*
+  問題：
+  1 ~ n の番号がついた n 種類のおもりがあり、おもり i の重さは a_i です。
+  それぞれのおもりは無限個存在しており、任意のおもりを任意の個数使うことができます。
+
+  このとき、おもりを選んで重さの和を x となるようにすることができるかどうか判定してください。
+*/
+
+import java.util.*;
+public class dp_primer_partial_sums_boss_NG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        
+        int[] weights = new int[n];
+        
+        boolean[] dp = new boolean[x+1];
+        
+        Arrays.fill(dp, false);
+        
+        dp[0] = true;
+        
+        for(int i = 0; i<n; i++){
+            weights[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i<n; i++){
+            for(int j = x; j>=weights[i]; j--){
+
+                if(dp[j-weights[i]]==true){
+                    dp[j] = true;
+                }
+                
+                int ref = j-weights[i];
+                
+                for(int k = 0; k<n; k++){
+                  if(ref%weights[k] == 0){
+                    dp[j] = true;
+                  }
+                }
+            }
+        }
+        
+        for(int i = 0; i<dp.length; i++){
+            System.out.println("重さ" + i + "は" + dp[i]);
+        }
+
+        sc.close();
+    }
+}
