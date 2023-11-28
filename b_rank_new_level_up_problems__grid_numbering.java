923992f82d3c7e43d713238463b8598b18cf953f
田中冬紀 <winter.tanaka@gmail.com>
2023-11-28 14:45:29 +0900

    「【マップの扱い 4】マップのナンバリング」を追加、模範解答も記載

---
+++ b_rank_new_level_up_problems__grid_numbering.java
@@ -0,0 +1,112 @@
+/* 自分で解いたコード */
+/* これで正解 */
+/* 斜め方向の処理に関しては、模範解答よりも上手くできたかも */
+
+/*
+    問題：
+    マップの行数 H と列数 W とナンバリングの向き D が与えられるので、
+    (0, 0) から指示通りにナンバリングしたとき、マップ全体にどのように番号が振られるかを出力してください。
+
+    D=1...左下から右上への方向
+    D=2...左から右への横方向
+    D=3...上から下方向への縦方向
+    D=4...右上から左下への斜め方向
+*/
+
+import java.util.*;
+public class b_rank_new_level_up_problems__grid_numbering {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int d = sc.nextInt();
+        
+        int[][] array = new int[h][w];
+        
+        int a = 1;
+        
+        //ifやelse ifで書いてしまったが、switch文の方がわかりやすいと思われる
+        if (d == 1){
+            /* 
+                左下から右上への斜め方向に関しては、
+                斜め一列の各座標は、縦座標+横座標 の値が一致している。
+                この一致した値を変数sumとし、ループで処理していく。
+            */
+            for(int sum = 0; sum <= h + w - 2; sum++){
+                for(int j = 0; j < w; j++){
+                    
+                    //次の斜め一列へと移る
+                    if(sum - j < 0){
+                        break;
+                    }
+                    
+                    //範囲外の座標をマッピングしないようにする
+                    if(sum - j > h - 1){
+                        continue;
+                    }
+                    
+                    array[sum-j][j] = a;
+                    a++;
+                }
+            }
+            
+        } else if (d == 2){
+            //左から右への縦方向
+            for(int i = 0; i < h; i++){
+                for(int j = 0; j < w; j++){
+                    array[i][j] = a;
+                    a++;
+                }
+            }
+            
+        } else if (d == 3){
+            //上から下への縦方向
+            for(int i = 0; i < w; i++){
+                for(int j = 0; j < h; j++){
+                    array[j][i] = a;
+                    a++;
+                }
+            }
+            
+        } else {
+            /* 
+                右上から左下への斜め方向に関しても、
+                斜め一列の各座標は、縦座標+横座標 の値が一致している。
+                この一致した値を変数sumとし、ループで処理していく。
+
+                基本的には、左下から右上への斜め方向の処理と同じ(縦と横が入れ替わっただけ)
+            */
+            for(int sum = 0; sum <= h + w - 2; sum++){
+                for(int j = 0; j < h; j++){
+                    
+                    if(sum - j < 0){
+                        break;
+                    }
+                    
+                    if(sum - j > w - 1){
+                        continue;
+                    }
+                    
+                    array[j][sum-j] = a;
+                    a++;
+                }
+            }
+        }
+        
+        //結果を表示していく
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                System.out.print(array[i][j]);
+                if(j == w-1){
+                    System.out.println();
+                }else{
+                    System.out.print(" ");
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ b_rank_new_level_up_problems__grid_numbering_model_answer.java
@@ -0,0 +1,109 @@
+/* 模範解答 */
+
+/*
+    問題：
+    マップの行数 H と列数 W とナンバリングの向き D が与えられるので、
+    (0, 0) から指示通りにナンバリングしたとき、マップ全体にどのように番号が振られるかを出力してください。
+
+    D=1...左下から右上への方向
+    D=2...左から右への横方向
+    D=3...上から下方向への縦方向
+    D=4...右上から左下への斜め方向
+*/
+
+/*
+    解説：
+    マップを範囲外参照しないように注意しつつ、二重ループなどを用いてナンバリングをおこないましょう。
+    地道に手を動かして添字の動きを確認しましょう。
+
+    斜め方向のナンバリングでは、各斜め列の最初の要素に注目してから斜めに移動していくイメージを持つとナンバリングのイメージがしやすいと思います
+*/
+
+import java.util.*;
+
+public class b_rank_new_level_up_problems__grid_numbering_model_answer {
+    public static void main(String... args) {
+        
+        var sc = new Scanner(System.in);
+        
+        var H = sc.nextInt();
+        var W = sc.nextInt();
+        var D = sc.nextInt();
+        
+        var count = 1;
+        var ans = new int[H][W];
+        
+        switch(D) {
+            
+            case 1 -> {
+                ans[0][0] = 1;
+                count++;
+                
+                for (var i = 1; i < H; i++) {
+                    for (int j = 0; j <= Math.min(i, W - 1); j++) {
+                        ans[i - j][j] = count;
+                        count++;
+                    }
+                }
+                
+                for (var i = 1; i < W; i++) {
+                    for (var j = 0; j < Math.min(H, W - i); j++) {
+                        ans[H - 1 - j][i + j] = count;
+                        count++;
+                    }
+                }
+            }
+            
+            case 2 -> {
+                for (var i = 0; i < H; i++) {
+                    for (var j = 0; j < W; j++) {
+                        ans[i][j] = count;
+                        count++;
+                    }
+                }
+            }
+            
+            case 3 -> {
+                for (var i = 0; i < W; i++) {
+                    for (var j = 0; j < H; j++) {
+                        ans[j][i] = count;
+                        count++;
+                    }
+                }
+            }
+            
+            case 4 -> {
+                ans[0][0] = 1;
+                count++;
+                
+                for (var i = 1; i < W; i++) {
+                    for (var j = 0; j <= Math.min(i, H - 1); j++) {
+                        ans[j][i - j] = count;
+                        count++;
+                    }
+                }
+                
+                for (var i = 1; i < H; i++) {
+                    for (var j = 0; j < Math.min(H - i, W); j++) {
+                        ans[i + j][W - 1 - j] = count;
+                        count++;
+                    }
+                }
+            }
+            
+        }
+        
+        for (var i = 0; i < H; i++) {
+            var sb = new StringBuilder();
+            for (var j = 0; j < W; j++) {
+                if (j != 0) {
+                    sb.append(" ");
+                }
+                sb.append(ans[i][j]);
+            }
+            System.out.println(sb);
+        }
+
+        sc.close();
+    }
+}
